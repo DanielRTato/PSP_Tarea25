ce8bd6f60067a03934567dcc623f037b762e987d
DanielRTato <drodrigueztato@danielcastelao.org>
2025-11-07 19:54:12 +0100

    cambiado a try-with-resources,

--- src/ConexionSocket.java
+++ src/ConexionSocket.java
@@ -4,11 +4,8 @@ import java.net.Socket;
 public class ConexionSocket {
 
     public void establecerConexion(String ip, int puerto) {
-        try {
-            Socket socket = new Socket(ip,puerto);
-
+        try(Socket socket = new Socket(ip, puerto)) {
             System.out.println("El puerto: " + puerto + " está abierto - " + socket.getInetAddress());
-
         } catch (IOException e) {
             System.out.println("El puerto: " + puerto + " está cerrado");
         }
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         ConexionSocket cst = new ConexionSocket();
 
         while (true) {
-            System.out.print("Introduce la dirección IP(o localhost), o escribe 'salir' para terminar. ");
+            System.out.print("Introduce la dirección IP (o 'localhost'), o escribe 'salir' para terminar. ");
             String direccion = scanner.nextLine().toLowerCase();
 
             if (direccion.equals("salir")){
@@ -18,6 +18,7 @@ public class Main {
             System.out.println("1 - Probar los puertos famosos (21, 22, 80, 443)");
             System.out.println("2 - Probar un puerto específico");
             int opcion = scanner.nextInt();
+            scanner.nextLine();
 
             if (opcion == 1) {
                 int[] puertosFamosos = {21, 22, 80, 443};
@@ -28,12 +29,15 @@ public class Main {
             } else if (opcion == 2) {
                 System.out.print("Introduce el puerto que quieres probar:");
                 int puerto = scanner.nextInt();
+                scanner.nextLine();
 
                 cst.establecerConexion(direccion, puerto);
 
             } else {
                 System.out.println("Opción incorrecta, intentalo otra vez");
             }
+
+            System.out.println("--------------------------------------");
         }
         scanner.close();
     }
